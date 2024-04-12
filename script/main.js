f139bc09d756f2408fbb5ba5af38c2b59a0efb3c
idigrow/ytj <wnaj55@idigrow.com>
2024-04-12 18:29:41 +0900

    0412 commit

--- script/main.js
+++ script/main.js
@@ -11,9 +11,7 @@ var scope = [
 const imgRight = document.querySelector(".scroll-right");
 const imgLeft = document.querySelector(".scroll-left");
 
-// 실행
-window.addEventListener("scroll", handScroll);
-
+//PC,Tablet
 function handScroll() {
 	//문서의 전체 높이
 	var documentHeight = $(document).height();
@@ -59,7 +57,10 @@ function handScroll() {
 	if (scrolledPercentage > 44) {
 		$(".scroll-left").css("left", "-50%");
 		$(".scroll-right").css("right", "-70%");
-	} 
+		$("#main .main-content").css("z-index","1");
+	} else {
+		$("#main .main-content").css("z-index","2");
+	}
 
 	// gnb 클래스를 가진 요소 선택
 	var gnb = document.querySelector('.gnb');
@@ -88,6 +89,93 @@ function handScroll() {
 
 }
 
+// 모바일
+function handScroll_m() {
+	//문서의 전체 높이
+	var documentHeight = $(document).height();
+	//브라우저 창 높이
+	var windowHeight = $(window).height();
+
+	//현재 페이지에서 스크롤된 백분율
+	var scrolledPercentage = Math.ceil(($(this).scrollTop() / (documentHeight - windowHeight)) * 100);
+
+	console.log("스크롤된 비율: " + scrolledPercentage + "%");
+
+	// 프리다, 로고 움직이기
+	if (scrolledPercentage < 11) {
+		$(".scroll-left").css("left", "-30%");
+		$(".scroll-right").css("right", "-67%");
+		$(".scroll-up").css("bottom", "5%");
+	}
+	if (scrolledPercentage > 10 && scrolledPercentage < 19) {
+		$(".scroll-left").css("left", "-50%");
+		$(".scroll-right").css("right", "-87%");
+		$(".scroll-up").css("bottom", "40%");
+	}
+	if (scrolledPercentage > 18 && scrolledPercentage < 27) {
+		$(".scroll-left").css("left", "-70%");
+		$(".scroll-right").css("right", "-100%");
+		$(".scroll-up").css("bottom", "80%");
+	}
+	if (scrolledPercentage > 26) {
+		$(".scroll-left").css("left", "-80%");
+		$(".scroll-right").css("right", "-110%");
+		$(".scroll-up").css({
+			"bottom":"90%",
+			"left":"20%",
+		});
+		setTimeout(()=>{
+			$(".scroll-up").css("opacity","0");
+		},100);
+		$("#header").fadeIn();
+		$("h1").fadeIn();
+	} else {
+		$("#header").fadeOut();
+		$("h1").fadeOut();
+		$(".scroll-up").css({
+			"opacity":"1",
+			"left":"50%"
+		});
+		
+	}
+	if (scrolledPercentage > 28) {
+		$(".scroll-left").css("left", "-100%");
+		$(".scroll-right").css("right", "-130%");
+		$("#main .main-content").css("z-index","1");
+	} else {
+		$("#main .main-content").css("z-index","2");
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// 실행
+	if(document.body.clientWidth < 768 ) {
+		window.addEventListener("scroll", handScroll_m);
+	} else {
+		window.addEventListener("scroll", handScroll);
+	}
+
+	
+	const navInput = document.querySelector('.hambuger input');
+	const hambuger = document.querySelector('.hambuger');
+	const gnb = document.querySelector('.gnb');
+	navInput.addEventListener('change',function(){
+		if(this.checked) {
+			gnb.classList.add('active');
+			hambuger.classList.add('active');
+			document.getElementById('header').style.zIndex = '11';
+		} else {
+			gnb.classList.remove('active');
+			hambuger.classList.remove('active');
+			setTimeout(()=>{
+				document.getElementById('header').style.zIndex = '10';
+			},500);
+			
+		}
+	});	
+});
+
 
 //타이핑 효과
 const typeWraps = document.querySelectorAll('.type-wrap');
@@ -99,7 +187,7 @@ function showNextSpan(typeWrapIndex, spanIndex) {
 			setTimeout(() => {
 				spans[spanIndex].classList.add('show');
 				showNextSpan(typeWrapIndex, spanIndex + 1);
-			}, 1000); // 1초 후에 다음 span 보이기
+			}, 500); // 1초 후에 다음 span 보이기
 		} else {
 			// 현재 type-wrap의 모든 span이 보여지면 다음 type-wrap으로 이동
 			showNextSpan(typeWrapIndex + 1, 0);
